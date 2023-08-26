db935df8a391a7698e7c69412f049a21190d5c12
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2023-08-26 20:05:33 -0300

    Arrumando o exercicio

--- Aula02/exercicio2.js
+++ Aula02/exercicio2.js
@@ -12,20 +12,27 @@ let matriz2 = [[2, 2],
                [0, 1]];
 
 function multiplicarMatrizes(matriz1, matriz2){
-    let multiplicada = [];
-
-    for(let i = 0; i < matriz1.length; i++){
-        let linha = [];
-        for(let j = 0; j < matriz1[i].length; j++){
-            if(matriz1[i].length != matriz2[j].length){
-                console.log("Não é possível calcular")
-                return "ERRO";
-            } else{
-                linha.push(matriz1[i][j] * matriz2[j][i]);
-            }
-        }
-        multiplicada.push(linha);
+    const linhas1 = matriz1.length;
+    const colunas1 = matriz1[0].length;
+    const linhas2 = matriz2.length;
+    const colunas2 = matriz2[0].length;
+  
+    if (colunas1 !== linhas2 || linhas1 !== colunas2) {
+        return "Deu ruim!";
     }
+  
+    const multiplicada = Array(linhas1);
+    for (let i = 0; i < linhas1; i++) {
+      multiplicada[i] = Array(colunas2);
+      for (let j = 0; j < colunas2; j++) {
+        const posi = [];
+        for (let k = 0; k < colunas1; k++) {
+        posi.push(matriz1[i][k] + "*" + matriz2[k][j]);
+        multiplicada[i][j] = posi.join(" + ");
+        }
+      }
+    }
+  
 
     return multiplicada;
 
