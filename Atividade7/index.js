d7cdba25a1d9234b995c325ac22b54b726d243f1
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2023-10-12 13:54:06 -0300

    Atividade 7

--- Atividade7/index.js
+++ Atividade7/index.js
@@ -1,24 +1,73 @@
 const express = require('express');
-const mustache = require('mustache-express'); 
+const mustacheExpress = require('mustache-express'); // framework para renderizacao de pagina html
 const app = express();
 
-app.engine('html', mustache()); 
-app.set('view engine', 'html'); 
-app.set('views', __dirname + '/views'); 
+app.engine('html', mustacheExpress()); // criando a engine para renderizacao
+app.set('view engine', 'html'); // setando a engine 'html' para ser usada
+app.set('views', __dirname + '/views'); // especificando o caminho das views
 
 const PORT = 8080;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index.html'));
 
 app.post('/dados', (req, res) => {
-    const {name, address, telefone, date} = req.body;
-    res.render('dados.html', {name, address, telefone, date});
+    const paciente = {
+        name: req.body.name,
+        cpf: req.body.cpf,
+        birthDate: req.body.birthDate,
+        address: req.body.address,
+        uf: req.body.uf,
+        cep: req.body.cep,
+        email: req.body.email,
+        telefone: req.body.telefone,
+    };
+
+    const consulta = {
+        doctor: req.body.doctor,
+        dateTime: req.body.dateTime,
+        drug: req.body.drug,
+        textInfo: req.body.textInfo,
+    };
+
+    const fieldsName = {
+        name: "Nome Completo",
+        cpf: "CPF",
+        uf: "UF",
+        cep: "CEP",
+        medicine: "Selecione o Medicamento Alérgico",
+        email: "Email",
+        telefone: "Telefone",
+        doctor: "Selecione o Especialista",
+        birthDate: "Data de Nascimento",
+        address: "Endereço",
+        dateTime: "Data e Hora",
+        textInfo: "Informações Adicionais"
+    }
+
+    let erro_form = [];
+
+    Object.entries(dadosPaciente).forEach(info => {
+        if(info[1] == "") {
+            const fieldWithError = fieldsName[info[0]]
+            console.log('dados do paciente', fieldWithError)
+            erro_form.push(fieldWithError)
+        }
+    });
+
+    Object.entries(dadosConsulta).forEach(info => {
+        if(info[1] == "") {
+            const fieldWithError = fieldsName[info[0]]
+            console.log('dados da consulta', fieldWithError)
+            erro_form.push(fieldWithError)
+        }
+    });
+
+    console.log(erro_form);
+
+    res.render("dados.html", {dadosPaciente, dadosConsulta, erro_form});
+
 });
 
-app.listen(PORT, function(){
-    console.log(`App rodando na porta ${PORT}`);
-})
-
-
+app.listen(PORT, () => console.log(`app rodando na porta ${PORT}`));
